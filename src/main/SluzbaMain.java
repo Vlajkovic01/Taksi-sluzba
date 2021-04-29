365380bab985cf80e6ab0db15d26e4fee770a1af
Stefan Vlajkovic <vlajkovicstefan01@gmail.com>
2021-04-29 21:30:02 +0200

    update uml diagram and added some new test file

--- src/main/SluzbaMain.java
+++ src/main/SluzbaMain.java
@@ -23,23 +23,26 @@ public class SluzbaMain {
         System.out.println("Dodavanje test podataka, testiranje upisivanja u fajl...");
 
         Automobil testAutomobil = new Automobil("TestMarka","TestProizvodjac",2021,"TestReg",108, TipVozila.KOMBI,false,false);
-        sluzba.dodajAutomobil(testAutomobil);
 
         Dispeceri testDispecer = new Dispeceri("test", "test","test","test","test","test", Pol.MUSKO,"test", TipKorisnika.DISPECER,false,5,50000,"123", Odeljenje.PRIJEM_VOZNJI);
-        sluzba.dodajDispecera(testDispecer);
 
         Vozaci testVozac = new Vozaci("test","test","test","test","test","test",Pol.MUSKO,"test",TipKorisnika.VOZAC,false,10,5000,555,testAutomobil, new ArrayList<Voznja>());
+
         Musterije testMusterija = new Musterije("test", "test","test","test","test","test", Pol.MUSKO,"test", TipKorisnika.MUSTERIJA,false,5,new ArrayList<Voznja>());
+
         Voznja testVoznja = new Voznja(10,"10-10-2010/22:00","test","test",testMusterija,testVozac,100,30, StatusVoznje.PRIHVACENA);
+
+        System.out.println("Snimanje novih podataka...\n");
 
         testMusterija.getVoznjeMusterije().add(testVoznja);
         testVozac.getVoznjeVozaca().add(testVoznja);
 
+        sluzba.dodajAutomobil(testAutomobil);
+        sluzba.dodajDispecera(testDispecer);
         sluzba.dodajMusteriju(testMusterija);
         sluzba.dodajVozaca(testVozac);
         sluzba.dodajVoznju(testVoznja);
 
-        System.out.println("Snimanje novih podataka...");
         sluzba.snimiAutomobile();
         sluzba.snimiDispecere();
         sluzba.snimiMusterije();
